c050b38b4dcf8a531ffaa74cf544b0a0262a411e
andreicalinciuc <scoala.andrei@yahoo.com>
2021-05-26 11:25:10 +0300

    install toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {LoginProvider} from "./components/auth/AuthContext";
 import AppRouter from "./router/AppRouter";
